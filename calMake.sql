eaf76f31ae908693ef63fe278bca6c9b66680b0a
František Sedláček <pida@cica.cz>
2016-09-04 15:35:59 +0200

    Regenerated calMake.sql
    Oprava drobných chyb ve schématu, vytvořeném původním .sql souborem.

--- calMake.sql
+++ calMake.sql
@@ -5,31 +5,32 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema calendar
 -- -----------------------------------------------------
--- Kalendář událostí
+DROP SCHEMA IF EXISTS `calendar` ;
 
 -- -----------------------------------------------------
 -- Schema calendar
---
--- Kalendář událostí
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `calendar` DEFAULT CHARACTER SET utf8 ;
 USE `calendar` ;
 
 -- -----------------------------------------------------
--- Table `calendar`.`table1`
+-- Table `calendar`.`udalosti`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `calendar`.`table1` (
+DROP TABLE IF EXISTS `calendar`.`udalosti` ;
+
+CREATE TABLE IF NOT EXISTS `calendar`.`udalosti` (
   `Date` DATE NOT NULL,
   `Name` VARCHAR(45) NOT NULL,
-  `Description` VARCHAR(100) NULL,
+  `Description` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`Date`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-CREATE USER 'calUser' IDENTIFIED BY '123456';
-
-GRANT ALL ON `calendar`.* TO 'calUser';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
